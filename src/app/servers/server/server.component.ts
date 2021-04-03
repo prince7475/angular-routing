3f234d51fb9a1b8109f6be7a098ce766e9b9fb5f
Prince Donkor <prince.donkor74@gmail.com>
2021-04-03 18:56:57 -0500

    nested routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -17,10 +17,8 @@ export class ServerComponent implements OnInit {
     const id = +this.route.snapshot.params['id']
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
-      console.log(`params`, params['id'])
       if(params?.id){
         this.server = this.serversService.getServer(+params.id);
-        console.log(`this.server `, this.server )
       }else {
         this.server = this.serversService.getServer(1);
       }
